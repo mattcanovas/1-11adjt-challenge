ec81dd517d8040946def574cbd0fc1bb4546514c
Matheus Canovas <matheuscanovas9@gmail.com>
2026-01-20 09:22:44 -0300

    chore: Criando projeto e artefatos

---
+++ gfood-api/src/test/java/br/com/fiap/gfood/api/GfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gfood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
