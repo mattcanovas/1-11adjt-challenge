515ee1be14e7c3a551a51db2384aa9fad09e1b2c
Matheus Canovas <matheuscanovas9@gmail.com>
2026-01-21 09:02:05 -0300

    wip: Limpando projeto

--- gfood-api/src/main/java/br/com/fiap/gfood/api/entities/Type.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.gfood.api.entities;
-
-public enum Type
-{
-	DEFAULT, OWNER
-}
--- gfood-api/src/main/java/br/com/fiap/gfood/api/exceptions/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.gfood.api.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class GlobalExceptionHandler
-{
-	
-}
